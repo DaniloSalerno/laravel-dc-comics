d9f3f04a1dcacef867dbc9dc68af69f6e5ef45e0
DaniloSalerno <salerno.danilo@hotmail.it>
2023-10-31 17:14:31 +0100

    add index page for admin

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Guest\PageController;
+use App\Http\Controllers\Admin\ComicController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,5 +16,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [PageController::class, 'index'])->name('home');
-
 Route::get('/comics', [PageController::class, 'comics'])->name('comics');
+
+
+
+Route::resource('admin/comics', ComicController::class);
