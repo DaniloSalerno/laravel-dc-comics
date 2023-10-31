6bc048ec5e16cb44cf6ff74720bb95d9e9eeaf0d
DaniloSalerno <salerno.danilo@hotmail.it>
2023-10-31 16:50:06 +0100

    fix ComicSeeder and Migrations

--- database/seeders/ComicSeeder.php
+++ database/seeders/ComicSeeder.php
@@ -17,13 +17,13 @@ class ComicSeeder extends Seeder
 
         foreach ($comics as $comic) {
             $new_comic = new Comic();
-            $new_comic->title = $comic->title;
-            $new_comic->description = $comic->description;
-            $new_comic->price = $comic->price;
-            $new_comic->series = $comic->series;
-            $new_comic->sale_date = $comic->sale_date;
-            $new_comic->artists =  implode(",",  $comic->artists);
-            $new_comic->writers = implode(",",  $comic->writers);
+            $new_comic->title = $comic['title'];
+            $new_comic->description = $comic['description'];
+            $new_comic->price = $comic['price'];
+            $new_comic->series = $comic['series'];
+            $new_comic->sale_date = $comic['sale_date'];
+            $new_comic->artists =  implode(",",  $comic['artists']);
+            $new_comic->writers = implode(",",  $comic['writers']);
             $new_comic->save();
         }
     }
